0d262c82315bdd02dbf0f1901da9e6aeefbf2396
Andrew David Burt <andrew@ampletorque.com>
2018-04-24 21:02:22 +0000

    expose error msg function correctly

--- middleware/messages.js
+++ middleware/messages.js
@@ -9,4 +9,14 @@ function message(req) {
         };
 };
 
-res.error = msg => this.message(msg, 'error');
+module.exports = (req, res, next) => {
+        res.message = message(req);
+        res.error = (msg) => {
+                return res.message(msg, 'error');
+        };
+        res.locals.messages = req.session.messages || [];
+        res.locals.removeMessages = () => {
+                req.session.messages = [];
+        };
+        next();
+};
